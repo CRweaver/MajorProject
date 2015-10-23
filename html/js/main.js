35f5fcfc8415e99f0def2869b6d6248df0481f66
CRweaver <claireweaver@Claires-MacBook-Pro.local>
2015-10-23 15:27:25 +1100

    Errors in responsive nav, and slider bugged out so had to delete. Happy to resubmit after you have seen it and provided some feedback.

---
+++ html/js/main.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+    $(document).ready(function () {
+        
+        /* This is where we initialise our carousel */
+        $('.carousel').slick({
+            displayArrows: true,
+            arrows: true,
+            slidesToShow: 3
+        });
+        
+/*global $, console*/
+(function () {
+    'use strict';
+    $(document).ready(function () {
+        /* Find the hamburger li and attach a click handler */
+        $('nav ul:first-child').click(function () {
+            /* toggle the class menu-expanded on the ul
+            ** be careful with the "this" context as our selector is a pseudo element and "this" refers to the ul
+            **/
+            $(this).toggleClass('menu-expanded');
+        });
+    });
+}());
